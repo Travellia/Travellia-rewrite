f8467bf6579273f6fef831d9e2403437fdc36267
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-07 17:20:55 +0500

    refactor(PlanYourTrip): create BookingForm and ContactUs(schema) components

--- src/components/holidaypackagepage/PlanYourTrip/index.jsx
+++ src/components/holidaypackagepage/PlanYourTrip/index.jsx
@@ -1,6 +1,7 @@
 import ContentLayoutWrapper from "@/components/common/ContentLayoutWrapper";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import BookingForm from "./BookingForm";
 
 const Index = () => {
   return (
@@ -14,86 +15,7 @@ const Index = () => {
 
         {/* Form */}
         <div className="w-full">
-          <form className="">
-            <div className="grid grid-cols-2 gap-4 mb-4 w-full">
-              <div className="">
-                <label
-                  htmlFor="firstName"
-                  className="block text-sm font-bold pl-5 text-gray-500 mb-1"
-                >
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  id="firstName"
-                  placeholder="Your First name..."
-                  className="w-full p-5 bg-white rounded-md"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="lastName"
-                  className="block text-sm font-bold pl-5 text-gray-500 mb-1"
-                >
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  id="lastName"
-                  placeholder="Your Last name..."
-                  className="w-full p-5 bg-white rounded-md"
-                />
-              </div>
-            </div>
-            <div className="grid grid-cols-2 gap-4 mb-4 w-full">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-bold pl-5 text-gray-500 mb-1"
-                >
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="name@travellia.com"
-                  className="w-full p-5 bg-white rounded-md"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="phone"
-                  className="block text-sm font-bold pl-5 text-gray-500 mb-1"
-                >
-                  Phone Number
-                </label>
-                <input
-                  type="tel"
-                  id="phone"
-                  placeholder="+44 55 66 77 88"
-                  className="w-full p-5 bg-white rounded-md"
-                />
-              </div>
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="instructions"
-                className="block text-sm font-bold pl-5 text-gray-500 mb-1"
-              >
-                Booking Instructions
-              </label>
-              <textarea
-                id="instructions"
-                placeholder="Booking Instructions..."
-                className="w-full p-5 bg-white rounded-md h-40"
-              ></textarea>
-            </div>
-            <div className="flex justify-center py-6">
-              <Button type="submit" class="w-auto btn-main ">
-                Booking Instructions
-              </Button>
-            </div>
-          </form>
+          <BookingForm />
         </div>
       </ContentLayoutWrapper>
     </section>
