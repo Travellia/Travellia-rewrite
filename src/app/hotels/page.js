5f190cd957f89be5938e109b87f3669e12a61da5
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-08 13:22:53 +0500

    feat(Welcome): complete welcomepage

---
+++ src/app/hotels/page.js
@@ -0,0 +1,12 @@
+import Welcome from "@/components/hotel/Welcome";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col">
+      <Welcome />
+    </div>
+  );
+};
+
+export default page;
