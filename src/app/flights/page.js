d4adfb4e357c83e8bbe314d6f712605e8820ff54
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-19 12:29:38 +0500

    feat(FlightPage): setup page and make AirFlight component (With responsiveness)

--- src/app/flights/page.js
+++ src/app/flights/page.js
@@ -1,5 +1,6 @@
-import FilterSearch from "@/components/flight/FilterSearch";
 import Welcome from "@/components/flight/welcome";
+import FilterSearch from "@/components/flight/FilterSearch";
+import MainContent from "@/components/flight/MainContent";
 import React from "react";
 
 const page = () => {
@@ -8,6 +9,7 @@ const page = () => {
       <Welcome />
       <div className="flex flex-col gap-7 xl:gap-10 -translate-y-10  md:-translate-y-40 lg:-translate-y-32 xl:-translate-y-50 z-1 -mb-20">
         <FilterSearch defaultTab="flights" />
+        <MainContent />
       </div>
     </div>
   );
---
+++ src/components/flight/MainContent/LeftMenu/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AirFlight from "./AirFlight";
+
+const LeftMenu = () => {
+  return (
+    <section className="">
+      <AirFlight />
+    </section>
+  );
+};
+
+export default LeftMenu;
