58f44d40594ec6ea2457a8c17be39a28695965fe
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-19 13:06:09 +0500

    feat(FlightPage): Make CallUs Component with responsiveness

--- src/components/flight/MainContent/LeftMenu/index.jsx
+++ src/components/flight/MainContent/LeftMenu/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import AirFlight from "./AirFlight";
+import CallUs from "./CallUs";
 
 const LeftMenu = () => {
   return (
     <section className="">
-      <AirFlight />
+      <div className="flex flex-col gap-10">
+        <AirFlight />
+        <CallUs />
+      </div>
     </section>
   );
 };
