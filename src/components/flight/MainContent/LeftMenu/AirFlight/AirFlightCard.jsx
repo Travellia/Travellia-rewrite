48511918bc2d1730a75d881c0007f277382155ab
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-21 20:31:06 +0500

    fix: AirFlightCard in flight page

--- src/components/flight/MainContent/LeftMenu/AirFlight/AirFlightCard.jsx
+++ src/components/flight/MainContent/LeftMenu/AirFlight/AirFlightCard.jsx
@@ -5,9 +5,14 @@ import React from "react";
 
 const AirFlightCard = ({ data }) => {
   return (
-    <Card className="relative bg-secondary rounded-4xl px-6 py-5  flex-flex-col gap-3 shadow-lg">
+    <Card className="relative bg-secondary rounded-4xl px-6 py-5  flex-flex-col gap-3 shadow-lg ">
       {/* Floating Header */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-primary text-white font-semibold px-6 py-2 rounded-full text-sm shadow">
+      <div
+        className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2
+                bg-primary text-white font-semibold
+                px-6 py-2 rounded-full text-sm shadow
+                whitespace-nowrap"
+      >
         {data.route}
       </div>
 
@@ -26,7 +31,7 @@ const AirFlightCard = ({ data }) => {
         <div className=" rotate-180 block w-[80%] xl:w-px h-0.5 xl:h-40 bg-gray-400/40 " />
 
         {/* Price Section */}
-        <div className="flex justify-evenly items-center gap-10 md:gap-20 xl:flex xl:flex-col xl:items-center xl:gap-4 lg:min-w-40">
+        <div className="flex justify-evenly items-center gap-10 md:gap-20 xl:flex xl:flex-col xl:items-center xl:gap-4 ">
           <div className="text-4xl font-bold ">{data.price}</div>
 
           <Button className="rounded-full px-6">Book Now</Button>
@@ -63,23 +68,30 @@ const AirFlightCard = ({ data }) => {
 /* Reusable Flight Row */
 const FlightRow = ({ data }) => {
   return (
-    <div className="flex flex-col gap-3 md:flex md:flex-row md:items-center md:gap-6">
-      <Image src={data.logo} alt="logo" width={80} height={80} loading="lazy" />
-
-      <div className="flex gap-5 text-center">
-        <FlightCol title={data.from.time} sub={data.from.location} />
-        <FlightCol title={data.stop.number} sub={data.stop.location} />
-        <FlightCol title={data.to.time} sub={data.to.location} />
-        <div className="flex items-center text-sm font-medium text-gray-600">
-          {data.time}
-        </div>
+    <div className="grid grid-cols-5 items-center gap-6 w-full ">
+      {/* Logo column */}
+      <div className="flex justify-center">
+        <Image
+          src={data.logo}
+          alt="logo"
+          width={80}
+          height={80}
+          loading="lazy"
+        />
+      </div>
+      <FlightCol title={data.from.time} sub={data.from.location} />
+      <FlightCol title={data.stop.number} sub={data.stop.location} />
+      <FlightCol title={data.to.time} sub={data.to.location} />
+      {/* Duration */}
+      <div className="flex justify-center items-center text-sm font-medium text-gray-600 ">
+        {data.time}
       </div>
     </div>
   );
 };
 
 const FlightCol = ({ title, sub }) => (
-  <div className="flex flex-col items-center md:min-w-25 lg:min-w-15">
+  <div className="flex flex-col items-center justify-center w-full ">
     <div className="text-lg font-bold">{title}</div>
     <div className="text-sm font-medium text-gray-600">{sub}</div>
   </div>
