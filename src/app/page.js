f5afd0ef0ee60d581fb7cd9d10bb6f2f3b2bde92
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-21 17:13:19 +0500

    feat(homePage): change main imgae, add form

--- src/app/page.js
+++ src/app/page.js
@@ -9,21 +9,25 @@ import OfferedServices from "@/components/homepage/OfferedServices";
 import TrendingPackages from "@/components/homepage/TrendingPackages";
 import BookNow from "@/components/homepage/BookNow";
 import Testimonials from "@/components/homepage/Testimonial";
+import FilterSearch from "@/components/flight/FilterSearch";
 
 export default function Home() {
   return (
     <div className="flex flex-col bg-background">
       <Welcome />
-      <BestServices />
-      <Adventures />
-      <PopularDestination />
-      <UmrahPackages />
-      <SummerDeals />
-      <HotelBookings />
-      <OfferedServices />
-      <TrendingPackages />
-      <BookNow />
-      <Testimonials />
+      <div className="flex flex-col  -translate-y-10  md:-translate-y-40 lg:-translate-y-32 xl:-translate-y-50 z-1 -mb-20">
+        <FilterSearch />
+        <BestServices />
+        <Adventures />
+        <PopularDestination />
+        <UmrahPackages />
+        <SummerDeals />
+        <HotelBookings />
+        <OfferedServices />
+        <TrendingPackages />
+        <BookNow />
+        <Testimonials />
+      </div>
     </div>
   );
 }
