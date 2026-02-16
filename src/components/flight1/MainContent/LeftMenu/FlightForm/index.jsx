3084205c02ecb601509ba86828bd43aa31434838
codelearner-007 <farhanahmad0819@gmail.com>
2026-02-16 12:29:00 +0500

    fix/site: Just Change the path (temporary)

--- src/components/flight1/MainContent/LeftMenu/FlightForm/index.jsx
+++ src/components/flight1/MainContent/LeftMenu/FlightForm/index.jsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
-import Image from "next/image";
+import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import FlightFormSchema from "@/schemas/flight/FlightFormSchema";
 import { Button } from "@/components/ui/button";
