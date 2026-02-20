b4acd9815c556910bd09769c6adc51492c72dfb3
codelearner-007 <farhanahmad0819@gmail.com>
2026-02-20 21:08:11 +0500

    feat(umrahDetail): Book Now component

--- src/app/umrahDetail/page.js
+++ src/app/umrahDetail/page.js
@@ -1,6 +1,8 @@
+import BookNow from "@/components/common/BookNow";
 import FilterSearch from "@/components/common/FilterSearch";
 import Welcome from "@/components/common/Welcome";
 import HotelPackage from "@/components/umrahDetail/umrahPackage/HotelPackage";
+import { BookNowList } from "@/lib/data/umrahDetail/BookNowList";
 import { MADINAH_PACKAGE_DETAIL } from "@/lib/data/umrahDetail/MadinahHotelPackage copy";
 import { MAKKAH_PACKAGE_DETAIL } from "@/lib/data/umrahDetail/MakkahHotelPackage";
 import Image from "next/image";
@@ -16,36 +18,55 @@ const page = () => {
   const MAKKAH_HOTEL_PACKAGE = MAKKAH_PACKAGE_DETAIL;
   const MADINAH_HOTEL_PACKAGE = MADINAH_PACKAGE_DETAIL;
 
+  const imageData = {
+    image: "/umrahDetail/BookNow/BookNow-bg.png",
+    alt: "resturant",
+  };
+  const imageData2 = {
+    image: "/umrahDetail/BookNow/bookNow.png",
+    alt: "resturant",
+  };
+
+  const BOOKNOWLIST = BookNowList;
+
   return (
     <div className="flex flex-col">
       <Welcome data={welcomeData} />
       <div className="flex flex-col gap-7 xl:gap-10 -translate-y-10  md:-translate-y-40 lg:-translate-y-32 xl:-translate-y-50 z-1 -mb-20">
         <FilterSearch defaultTab={"umrah"} />
-        <div className="flex flex-col gap-7 xl:gap-10 relative">
-          <Image
-            src="/umrahDetail/umrahPackage/makkah.png"
-            alt="makkah"
-            height={1000}
-            width={1000}
-            className="hidden lg:block absolute bottom-0 right-0"
-          />
-          <Image
-            src="/umrahDetail/umrahPackage/madinah.png"
-            alt="makkah"
-            height={1000}
-            width={1000}
-            className="hidden lg:block absolute bottom-0 left-0"
-          />
-          <Image
-            src="/umrahDetail/umrahPackage/dot.png"
-            alt="makkah"
-            height={1000}
-            width={1000}
-            className="hidden lg:block absolute w-full right-0 -translate-y-10"
-          />
+        <div>
+          <div className="flex flex-col gap-7 xl:gap-20 relative">
+            <Image
+              src="/umrahDetail/umrahPackage/makkah.png"
+              alt="makkah"
+              height={1000}
+              width={1000}
+              className="hidden lg:block absolute bottom-0 right-0"
+            />
+            <Image
+              src="/umrahDetail/umrahPackage/madinah.png"
+              alt="makkah"
+              height={1000}
+              width={1000}
+              className="hidden lg:block absolute bottom-0 left-0"
+            />
+            <Image
+              src="/umrahDetail/umrahPackage/dot.png"
+              alt="makkah"
+              height={1000}
+              width={1000}
+              className="hidden lg:block absolute w-full right-0 -translate-y-10"
+            />
 
-          <HotelPackage data={MAKKAH_HOTEL_PACKAGE} />
-          <HotelPackage data={MADINAH_HOTEL_PACKAGE} reverse />
+            <HotelPackage data={MAKKAH_HOTEL_PACKAGE} />
+            <HotelPackage data={MADINAH_HOTEL_PACKAGE} reverse />
+            <BookNow
+              data={imageData}
+              data2={imageData2}
+              data3={BOOKNOWLIST}
+              reverse
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/common/BookNow.jsx
+++ src/components/common/BookNow.jsx
@@ -1,37 +1,112 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { MousePointer2 } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-const BookNow = ({ data }) => {
+const BookNow = ({ data, reverse, data2, data3 = [] }) => {
   return (
-    <section className="flex flex-col-reverse lg:grid lg:grid-cols-20 lg:min-h-[50vh] lg:max-h-[60vh] xl:min-h-[80vh]  xl:gap-10">
-      <div className="col-start-1 col-end-12 row-start-1 bg-primary rounded-t-[50px] lg:rounded-t-none lg:rounded-br-[100px] lg:rounded-tr-[100px]  z-50 flex items-center -mt-10 lg:mt-0 py-12 lg:py-10 xl:py-0 relative">
-        <div className="flex items-center justify-center  h-full w-full lg:mr-10 px-6 md:px-12 lg:px-0 ">
-          <div className="flex flex-col gap-4 xl:gap-10 text-center lg:text-left items-center lg:items-start">
-            <h1 className="text-3xl lg:text-5xl xl:text-7xl font-bold capitalize text-gray-200 pb-3">
-              Book now,
-              <br />
-              pay later
-            </h1>
-            <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200 max-w-[600px]">
-              Book Flights Now, Pay Later. Lock-in today's price
-              <br className="hidden md:block" />& pay in up to 26 weekly
-              payments.
-            </p>
-            <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200">
-              No interest, no credit checks & no hidden fees.
-            </p>
-            <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200">
-              Book your next trip
-            </p>
-            <Button className="btn-main-reverse">Book Now</Button>
-          </div>
+    <section className="flex flex-col-reverse lg:grid lg:grid-cols-20 lg:min-h-[50vh] lg:max-h-[60vh] xl:min-h-[80vh] xl:gap-10">
+      {/* ================= CONTENT SECTION ================= */}
+      <div
+        className={`
+    ${
+      reverse ? "lg:col-start-10 lg:col-end-21" : "lg:col-start-1 lg:col-end-12"
+    }
+    row-start-1
+    ${data2 ? "bg-[url('/umrahDetail/BookNow/bookNow.png')] bg-cover bg-center" : "bg-primary"}
+    rounded-t-[50px]
+    lg:rounded-t-none
+    ${
+      reverse
+        ? "lg:rounded-bl-[100px] lg:rounded-tl-[100px]"
+        : "lg:rounded-br-[100px] lg:rounded-tr-[100px]"
+    }
+    z-50
+    flex
+    items-center
+    -mt-10
+    lg:mt-0
+    py-12
+    lg:py-10
+    xl:py-0
+    relative
+  `}
+      >
+        <div
+          className={`flex items-center  h-full w-full px-6 md:px-12 ${data2 ? "justify-start" : "justify-center"}`}
+        >
+          {/* ===== CONDITION START ===== */}
+          {data2 ? (
+            // ✅ SHOW LIST SECTION
+            <div className="flex flex-col gap-6 xl:gap-10lg:items-start">
+              <h1 className="text-3xl lg:text-4xl xl:text-6xl font-bold capitalize text-primary pb-3">
+                WHY BOOK
+                <br />
+                WITH US:
+              </h1>
+
+              <div className="flex flex-col gap-3">
+                {data3.map((item, index) => (
+                  <div key={index} className="flex items-center gap-3">
+                    <MousePointer2 className="rotate-90 text-primary fill-primary" />
+                    <p className="text-black text-sm xl:text-lg font-semibold">
+                      {item}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            // ❌ SHOW DEFAULT SECTION
+            <div className="flex flex-col gap-4 xl:gap-10 text-center lg:text-left items-center lg:items-start">
+              <h1 className="text-3xl lg:text-5xl xl:text-7xl font-bold capitalize text-gray-200 pb-3">
+                Book now,
+                <br />
+                pay later
+              </h1>
+
+              <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200 max-w-[600px]">
+                Book Flights Now, Pay Later. Lock-in today's price
+                <br className="hidden md:block" />& pay in up to 26 weekly
+                payments.
+              </p>
+
+              <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200">
+                No interest, no credit checks & no hidden fees.
+              </p>
+
+              <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200">
+                Book your next trip
+              </p>
+
+              <Button className="btn-main-reverse">Book Now</Button>
+            </div>
+          )}
+          {/* ===== CONDITION END ===== */}
         </div>
       </div>
-      <div className="h-[40vh] sm:h-[50vh] lg:h-full w-full col-start-10 col-end-21 row-start-1 relative">
+
+      {/* ================= IMAGE SECTION ================= */}
+      <div
+        className={`
+          ${
+            reverse
+              ? "lg:col-start-1 lg:col-end-12"
+              : "lg:col-start-10 lg:col-end-21"
+          }
+          row-start-1
+          h-[40vh]
+          sm:h-[50vh]
+          lg:h-full
+          w-full
+          relative
+        `}
+      >
         <Image
-          src={data.image}
-          alt={data.alt}
+          src={data?.image}
+          alt={data?.alt}
           fill
           className="object-cover"
           loading="lazy"
